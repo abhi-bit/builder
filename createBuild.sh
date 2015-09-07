da9c1ef4980dfdf6464022163b1ff9182f8bfbf5
Abhishek Singh <singhabhishek.bit@gmail.com>
2015-09-07 12:21:14 +0530

    Using absolute paths

--- createBuild.sh
+++ createBuild.sh
@@ -7,11 +7,11 @@ buildXML=$3
 createbuild()
 {
     # list of commands to create build
-    ssh -p $port couchbase@localhost "mkdir builder"
-    ssh -p $port couchbase@localhost "cd builder"
-    ssh -p $port couchbase@localhost "cd builder; repo init -u git://github.com/couchbase/manifest -g all -m $buildXML"
-    ssh -p $port couchbase@localhost "cd builder; repo sync --jobs=20"
-    ssh -p $port couchbase@localhost "cd builder; cbbuild/scripts/jenkins/couchbase_server/server-linux-build.sh $OS 1010.0.0 enterprise 1"
+    ssh -p $port couchbase@localhost "mkdir /home/couchbase/builder"
+    ssh -p $port couchbase@localhost "cd /home/couchbase/builder"
+    ssh -p $port couchbase@localhost "cd /home/couchbase/builder; repo init -u git://github.com/couchbase/manifest -g all -m $buildXML"
+    ssh -p $port couchbase@localhost "cd /home/couchbase/builder; repo sync --jobs=20"
+    ssh -p $port couchbase@localhost "cd /home/couchbase/builder; cbbuild/scripts/jenkins/couchbase_server/server-linux-build.sh $OS 1010.0.0 enterprise 1"
     ssh -p $port couchbase@localhost "bash /home/couchbase/upload.sh"
     ssh -p $port couchbase@localhost "cd /home/couchbase/; rm -rf builder; rm -rf rpmbuild"
 }

a8bba8b5c32d31fba6b4b538eee252b4ab79004c
Abhishek Singh <abhishek@couchbase.com>
2015-09-08 23:11:38 +0530

    purging one of the duplicate rpm build command

--- createBuild.sh
+++ createBuild.sh
@@ -10,7 +10,7 @@ createbuild()
     ssh -p $port couchbase@localhost "mkdir ~/builder"
     ssh -p $port couchbase@localhost "cd ~/builder"
     ssh -p $port couchbase@localhost "cd ~/builder; repo init -u git://github.com/couchbase/manifest -g all -m $buildXML"
-    ssh -p $port couchbase@localhost "cd ~/builder; repo sync --jobs=20; cbbuild/scripts/jenkins/couchbase_server/server-linux-build.sh $OS 1010.0.0 enterprise 1"
+    ssh -p $port couchbase@localhost "cd ~/builder; repo sync --jobs=20"
     ssh -p $port couchbase@localhost "cd ~/builder; cbbuild/scripts/jenkins/couchbase_server/server-linux-build.sh $OS 1010.0.0 enterprise 1"
     ssh -p $port couchbase@localhost "bash ~/upload.sh"
     ssh -p $port couchbase@localhost "cd ~ ; rm -rf builder; rm -rf rpmbuild"

d144bc449a75a322db288e74377b9e0e1c2892f5
prataprc <prataprc@gmail.com>
2015-09-10 14:09:31 +0530

    updated builder script.
    - custom manifest repository.
    - queue multiple build jobs
    - list all jobs
    - persist builder state.
    - add command line parameters.

---
+++ config.go
@@ -0,0 +1,91 @@
+package main
+
+import "io/ioutil"
+import "log"
+import "encoding/json"
+import "sync"
+
+var config Config
+var muConfig sync.Mutex
+
+type Config struct {
+	filename        string
+	BuildId         int `json:"build_id"`
+	TotalBuilds     int `json:"total_builds"`
+	CompletedBuilds int `json:"completed_builds"`
+}
+
+func loadConfig(configfile string) {
+	muConfig.Lock()
+	defer muConfig.Unlock()
+
+	data, err := ioutil.ReadFile(configfile)
+	if err == nil {
+		err := json.Unmarshal(data, &config)
+		if err != nil {
+			log.Fatalf("error decoding config %v: %v", configfile, err)
+		}
+	} else {
+		config.BuildId = 0
+		config.TotalBuilds = 0
+		config.CompletedBuilds = 0
+	}
+	config.filename = configfile
+}
+
+func marshalConfig() ([]byte, error) {
+	muConfig.Lock()
+	defer muConfig.Unlock()
+
+	return json.Marshal(config)
+}
+
+func saveConfig() {
+	muConfig.Lock()
+	defer muConfig.Unlock()
+
+	data, err := json.Marshal(&config)
+	if err != nil {
+		log.Fatalf("error decoding config: %v", err)
+	}
+	err = ioutil.WriteFile(config.filename, data, 0660)
+	if err != nil {
+		log.Fatalf("error writing config %v: %v", config.filename, err)
+	}
+}
+
+func setConfig(key string, value interface{}) interface{} {
+	muConfig.Lock()
+	defer saveConfig()
+	defer muConfig.Unlock()
+
+	var old interface{}
+	switch key {
+	case "build_id":
+		old = config.BuildId
+		config.BuildId = value.(int)
+	case "total_builds":
+		old = config.TotalBuilds
+		config.TotalBuilds = value.(int)
+	case "completed_builds":
+		old = config.CompletedBuilds
+		config.CompletedBuilds = value.(int)
+	}
+	return old
+}
+
+func getConfig(key string) interface{} {
+	muConfig.Lock()
+	defer muConfig.Unlock()
+
+	var value interface{}
+	switch key {
+	case "build_id":
+		value = config.BuildId
+	case "total_builds":
+		value = config.TotalBuilds
+	case "completed_builds":
+		value = config.CompletedBuilds
+	}
+	return value
+}

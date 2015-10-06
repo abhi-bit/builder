3b093ef20572e0fc786e6cadbc792d00bc22bcee
Abhishek Singh <abhishek@couchbase.com>
2015-10-06 14:32:57 +0530

    Commiting changes for supporting builds for multiple OSes

--- upload.sh
+++ upload.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-cd $HOME/rpmbuild/RPMS/x86_64/
+packageDir=$1
+OS=$(lsb_release -d | awk '{print $2}')
 
-for file in $(ls .)
+if [ "$OS" == "CentOS" ]
+then
+        cd $HOME/rpmbuild/RPMS/x86_64/
+        ext="*.rpm"
+else
+        cd $packageDir/builder/
+        ext="*.deb"
+fi
+
+for file in $(ls $ext)
 do
     echo "Uploading to S3:" $file
     curl --upload-file $file http://s3.amazonaws.com/customers.couchbase.com/couchbase/
